c9c5e2caa654ae61748a5aec775098ae9103ca39
Douglas Valentin de Souza <90290847+Valentin-Douglas@users.noreply.github.com>
2021-09-23 18:44:57 -0300

    Updating Gym Quest - O primordio.py
    Just a few changes, since I'm advancing on classes

--- Gym Quest - O primordio.py
+++ Gym Quest - O primordio.py
@@ -4,24 +4,42 @@ print("Olá mundo!")
 
 # Primeiro as formalidades rs
 
-nome = input("Qual seu nome? ")
+nome = str(input('Qual seu nome? '))
 
-valor1 = input("Qual a sua idade? ")
-
-valor2 = input("Em que ano estamos? ")
-
-idade = int(valor2) - int(valor1)
-
-print(nome, ", você nasceu em ", idade)
+print(f'Muito prazer {nome}!')
 
 # Agora vamos conhecer nosso usuário
 
-print("Agora me conte um pouco mais de você!")
+print('Agora me conte um pouco mais de você!')
 
-altura = float(input("Qual a sua altura? "))
+altura = float(input('Qual a sua altura? '))
 
-peso = float(input("Qual o seu peso? "))
+peso = float(input('Qual o seu peso? '))
 
-IMC = (altura / peso) ** 2
+IMC = peso / (altura ** 2)
 
-print(f'Seu IMC é {IMC:.2}')
+if IMC < 16.00:
+    print('Você está na categoria Baixo peso - Grau III.')
+    print(f'Seu IMC é {IMC:.2}')
+elif IMC >= 16.00 and IMC < 16.99:
+    print('Você está na categoria Baixo peso - Grau II.')
+    print(f'Seu IMC é {IMC:.2}')
+elif IMC >= 17.00 and IMC < 18.49:
+    print('Você está na categoria Baixo peso - Grau I.')
+    print(f'Seu IMC é {IMC:.2}')
+elif IMC >= 18.50 and IMC < 24.99:
+    print('Você está na categoria Peso Ideal.')
+    print(f'Seu IMC é {IMC:.2}')
+elif IMC >= 25.00 and IMC < 29.99:
+    print('Você está na categoria Sobrepeso.')
+    print(f'Seu IMC é {IMC:.2}')
+elif IMC >= 30.00 and IMC < 34.99:
+    print('Você está na categoria Obesidade Grau I.')
+    print(f'Seu IMC é {IMC:.2}')
+elif IMC >= 35.00 and IMC < 39.99:
+    print('Você está na categoria Obesidade Grau II.')
+    print(f'Seu IMC é {IMC:.2}')
+else:
+    IMC > 40.00
+    print('Você está na categoria Obesidade Grau III.')
+    print(f'Seu IMC é {IMC:.2}')
